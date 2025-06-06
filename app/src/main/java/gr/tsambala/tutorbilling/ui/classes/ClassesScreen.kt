f671f0a2595685ea50ac79bd8f3f39c7f66d308f
tsampolion <dim.tsampallas@gmail.com>
2025-06-06 12:13:03 +0300

    Fix picker imports

--- app/src/main/java/gr/tsambala/tutorbilling/ui/classes/ClassesScreen.kt
+++ app/src/main/java/gr/tsambala/tutorbilling/ui/classes/ClassesScreen.kt
@@ -9,6 +9,7 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
