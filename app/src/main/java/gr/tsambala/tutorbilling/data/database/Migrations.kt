e7adafc229efec5b5a4def3baae66fcf70731d03
tsampolion <dim.tsampallas@gmail.com>
2025-06-08 19:24:46 +0300

    chore: bump app version to 0.5

--- app/src/main/java/gr/tsambala/tutorbilling/data/database/Migrations.kt
+++ app/src/main/java/gr/tsambala/tutorbilling/data/database/Migrations.kt
@@ -7,11 +7,18 @@ val MIGRATION_1_2 = object : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE students ADD COLUMN className TEXT NOT NULL DEFAULT 'Unassigned'")
     }
-    
+}
+
 val MIGRATION_2_3 = object : Migration(2, 3) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("ALTER TABLE students ADD COLUMN surname TEXT NOT NULL DEFAULT ''")
         database.execSQL("ALTER TABLE students ADD COLUMN parentMobile TEXT NOT NULL DEFAULT ''")
         database.execSQL("ALTER TABLE students ADD COLUMN parentEmail TEXT NOT NULL DEFAULT ''")
+    }
+}
+
+val MIGRATION_3_4 = object : Migration(3, 4) {
+    override fun migrate(database: SupportSQLiteDatabase) {
+        database.execSQL("ALTER TABLE students ADD COLUMN isActive INTEGER NOT NULL DEFAULT 1")
     }
 }
