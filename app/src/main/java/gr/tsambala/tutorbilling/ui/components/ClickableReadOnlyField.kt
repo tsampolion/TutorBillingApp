cfabea42f281440a7e53ea2d2401f9fa89274081
tsampolion <dim.tsampallas@gmail.com>
2025-06-11 02:27:14 +0300

    Fix unresolved reference build error

--- app/src/main/java/gr/tsambala/tutorbilling/ui/components/ClickableReadOnlyField.kt
+++ app/src/main/java/gr/tsambala/tutorbilling/ui/components/ClickableReadOnlyField.kt
@@ -2,7 +2,7 @@ package gr.tsambala.tutorbilling.ui.components
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.matchParentSize
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
@@ -27,7 +27,7 @@ fun ClickableReadOnlyField(
         )
         Box(
             Modifier
-                .matchParentSize()
+                .fillMaxSize()
                 .clickable(onClick = onClick)
         )
     }
