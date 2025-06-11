13bcdfdbaa386c7f826c324a78400b68dee17fcb
tsampolion <dim.tsampallas@gmail.com>
2025-06-11 03:10:19 +0300

    Annotate boolean defaults

--- app/src/main/java/gr/tsambala/tutorbilling/data/model/Student.kt
+++ app/src/main/java/gr/tsambala/tutorbilling/data/model/Student.kt
@@ -1,6 +1,7 @@
 // Student.kt - Fixed data model
 package gr.tsambala.tutorbilling.data.model
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -10,5 +11,6 @@ data class Student(
     val id: Long = 0,
     val name: String,
     val rate: Double, // Hourly rate in euros
+    @ColumnInfo(defaultValue = "1")
     val isActive: Boolean = true // Default to active
 )
